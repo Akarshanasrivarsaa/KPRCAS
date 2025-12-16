1b9d82afcbaf05067f8157075a50a7ab40a287a9
harini <sri25harini@gmail.com>
2025-12-16 14:13:04 +0530

    Express Created

---
+++ Backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const connectDb = async ()=>{
+     try{
+        await mongoose.connect("mongodb://localhost:27017/task");
+        console.log("MongoDB Connected🩵 🫣")
+     }catch(error){
+        console.log(error)
+     }
+}
+module.exports = connectDb;
